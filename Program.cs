d322429f8fe7440194fe5426f0547fe28cf6c90c
kaileyfreedman <kfreedman2023@chadwickschool.org>
2022-01-13 12:22:03 -0800

    worked on project

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace DataEncodingInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char ch;
+            ch = 'A';
+            Console.WriteLine($"ch is storing the value: {ch}");
+
+            int ch_decimal;
+            ch_decimal = ch;
+            Console.WriteLine($"The decimal value of ch is: {ch_decimal}");
         }
     }
 }

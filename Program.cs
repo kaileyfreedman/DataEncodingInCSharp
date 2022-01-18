2c91a6aab1dd01b0e5196c304f90a3c1bcca4517
kaileyfreedman <kfreedman2023@chadwickschool.org>
2022-01-18 10:29:34 -0800

    sign off

--- Program.cs
+++ Program.cs
@@ -6,33 +6,12 @@ namespace DataEncodingInCSharp
     {
         static void Main(string[] args)
         {
-            char ch;
-            ch = 'A';
-            Console.WriteLine($"ch is storing the value: {ch}");
+            Cipher cipher;
+            cipher = new Cipher(5);
 
-            int ch_decimal;
-            ch_decimal = ch;
-            Console.WriteLine($"The decimal value of ch is: {ch_decimal}");
-
-            int toConvert;
-            Console.WriteLine("Enter an integer: ");
-            toConvert = int.Parse(Console.ReadLine());
-
-            char asChar;
-            asChar = (char)toConvert;
-            Console.WriteLine($"The interger {toConvert} cast to char is '{asChar}'");
-
-            char b;
-            b = (char)('A' + 1);
-            Console.WriteLine($"'A' + 1 = '{b}'");
-
-            char z;
-            z = (char)('Z'+3);
-            Console.WriteLine($"'Z' + 3 = '{z}'");
-
-            char f;
-            f = (char)('F'-5);
-            Console.WriteLine($"'F' - 5 = '{f}'");
+            string encrypted;
+            encrypted = cipher.Encrypt("rosebud");
+            Console.WriteLine($"The encrypted message is: '{encrypted}'");
         }
     }
 }

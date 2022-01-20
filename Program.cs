bd3eb6ce188639616738d3395cd1d9b8ed0e4d24
kaileyfreedman <kfreedman2023@chadwickschool.org>
2022-01-19 16:26:29 -0800

    feat: Add Decrypt methodworked on project

--- Program.cs
+++ Program.cs
@@ -12,6 +12,17 @@ namespace DataEncodingInCSharp
             string encrypted;
             encrypted = cipher.Encrypt("rosebud");
             Console.WriteLine($"The encrypted message is: '{encrypted}'");
+            
+            string decrypted;
+            decrypted = cipher.Decrypt(encrypted);
+            Console.WriteLine($"The decrypted message is: '{decrypted};'");
+            
+            string message;
+            message = System.IO.File.ReadAllText("secrets/1.txt");
+            Console.WriteLine($"The encrypted message is: '{message}'."); 
+
+            
         }
     }
 }
+
